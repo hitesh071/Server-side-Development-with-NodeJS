6fec5a8543a1f00fc3cb935d82c0010c7dc17d03
Hitesh <gupta.hitesh786@gmail.com>
2020-05-17 23:17:13 +0530

    Assignment #3

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -22,7 +22,7 @@ dishRouter.route('/')
     },(err)=>next(err))
     .catch((err)=>next(err));
 })
-.post(authenticate.verifyUser,(req,res,next)=>{
+.post(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next)=>{
     Dishes.create(req.body)
     .then((dish)=>{
         console.log('Dish created ',dish);
@@ -32,11 +32,11 @@ dishRouter.route('/')
     },(err)=>next(err))
     .catch((err)=>next(err));
 })
-.put(authenticate.verifyUser,(req,res,next)=>{
+.put(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next)=>{
     res.statusCode=403;
     res.end('put operation not supported on /dishes');
 })
-.delete(authenticate.verifyUser,(req,res,next)=>{
+.delete(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next)=>{
     Dishes.remove({})
     .then((resp)=>{
         res.statusCode=200;
@@ -59,11 +59,11 @@ dishRouter.route('/:dishId')
     },(err)=>next(err))
     .catch((err)=>next(err));
 })
-.post(authenticate.verifyUser,(req,res,next)=>{
+.post(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next)=>{
     res.statusCode=403;
     res.end('POST operation not supported on /dishes/'+ req.params.dishId);
 })
-.put(authenticate.verifyUser,(req,res,next)=>{
+.put(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next)=>{
     Dishes.findByIdAndUpdate(req.params.dishId,{$set:req.body},{new:true})
     .then((dish)=>{
         res.statusCode=200;
@@ -72,7 +72,7 @@ dishRouter.route('/:dishId')
     },(err)=>next(err))
     .catch((err)=>next(err));
 })
-.delete(authenticate.verifyUser,(req,res,next)=>{
+.delete(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next)=>{
     Dishes.findByIdAndRemove(req.params.dishId)
     .then((resp)=>{
         res.statusCode=200;
@@ -136,7 +136,7 @@ dishRouter.route('/:dishId/comments')
     res.statusCode=403;
     res.end('put operation not supported on /dishes'+req.params.dishId+'/comments');
 })
-.delete(authenticate.verifyUser,(req,res,next)=>{
+.delete(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next)=>{
     Dishes.findById(req.params.dishId)
     .then((dish)=>{
         if(dish!=null){
@@ -194,7 +194,8 @@ dishRouter.route('/:dishId/comments/:commentId')
 .put(authenticate.verifyUser,(req,res,next)=>{
     Dishes.findById(req.params.dishId)
     .then((dish)=>{
-        if(dish!=null && dish.comments.id(req.params.commentId) != null){
+        if(dish!=null && dish.comments.id(req.params.commentId) != null
+        && dish.comments.id(req.params.commentId).author.equals(re.user_id)){
             if(req.body.rating){
                 dish.comments.id(req.params.commentId).rating=req.body.rating;
             }
@@ -218,9 +219,14 @@ dishRouter.route('/:dishId/comments/:commentId')
             err.status=404;
             return next(err);
         }
-        else{
+        else if (dish.comments.id(req.params.commentId)== null){
             err = new Error('Comment'+req.params.commentId+'not found');
             err.status=404;
+            return next(err);
+        }
+        else{
+            err=new Error('You are not authorized to update this comment.');
+            err.status=403;
             return next(err);
         }
         
@@ -230,7 +236,8 @@ dishRouter.route('/:dishId/comments/:commentId')
 .delete(authenticate.verifyUser,(req,res,next)=>{
     Dishes.findById(req.params.dishId)
     .then((dish)=>{
-        if(dish!=null && dish.comments.id(req.params.commentId)!=null){
+        if(dish!=null && dish.comments.id(req.params.commentId)!=null
+        && dish.comments.id(req.params.commentId).author.equals(req.user._id)){
             
             dish.comments.id(req.params.commentId).remove();
             dish.save()
@@ -250,9 +257,14 @@ dishRouter.route('/:dishId/comments/:commentId')
             err.status=404;
             return next(err);
         }
-        else{
+        else if (dish.comments.id(req.params.commentId)==null){
             err = new Error('Comment'+req.params.commentId+'not found');
             err.status=404;
+            return next(err);
+        }
+        else{
+            err=new Error('You are not authorized to delete this comment.');
+            err.status=403;
             return next(err);
         }
     },(err)=>next(err))
--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -20,7 +20,7 @@ promoRouter.route('/')
     },(err)=>next(err))
     .catch((err)=>next(err));
 })
-.post(authenticate.verifyUser,(req,res,next)=>{
+.post(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next)=>{
     Promotions.create(req.body)
     .then((promo)=>{
         //console.log('Promotion created '+ promo);
@@ -30,11 +30,11 @@ promoRouter.route('/')
     },(err)=>next(err))
     .catch((err)=>next(err));
 })
-.put(authenticate.verifyUser,(req,res,post)=>{
+.put(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,post)=>{
     res.statusCode=403;
     res.end('Put not supported on /promotions');
 })
-.delete(authenticate.verifyUser,(req,res,next)=>{
+.delete(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next)=>{
     Promotions.remove({})
     .then((resp)=>{
         res.statusCode=200;
@@ -55,11 +55,11 @@ promoRouter.route('/:promoId')
     },(err)=>next(err))
     .catch((err)=>next(err));
 })
-.post(authenticate.verifyUser,(req,res,next)=>{
+.post(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next)=>{
     res.statusCode=403;
     res.end('Post operation Not supported for /promotions/'+req.params.promoId);
 })
-.put(authenticate.verifyUser,(req,res,next)=>{
+.put(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next)=>{
     Promotions.findByIdAndUpdate(req.params.promoId,{$set:req.body},{new:true})
     .then((promo)=>{
         res.statusCode=200;
@@ -68,7 +68,7 @@ promoRouter.route('/:promoId')
     },(err)=>next(err))
     .catch((err)=>next(err));
 })
-.delete(authenticate.verifyUser,(req,res,next)=>{
+.delete(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next)=>{
     Promotions.findByIdAndDelete(req.params.promoId)
     .then((resp)=>{
         res.statusCode=200;
